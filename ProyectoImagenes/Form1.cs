c16fabd3d1b937c95a62abedac7b636909dd8a30
Mario Jose Roldan <dasdo1@gmail.com>
2020-07-09 23:57:21 -0600

    carga de imagen
    ya carga imagen

--- ProyectoImagenes/Form1.cs
+++ ProyectoImagenes/Form1.cs
@@ -16,5 +16,50 @@ namespace ProyectoImagenes
         {
             InitializeComponent();
         }
+
+        //leer matriz
+        Bitmap matriz = null;
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var d = new OpenFileDialog();
+            var resultado = d.ShowDialog();
+            if (resultado == System.Windows.Forms.DialogResult.Cancel)
+            {
+                return;
+            }
+            var filename = d.FileName;
+            var image = Image.FromFile(filename);
+            pictureBox1.Image = image;
+        }
     }
 }
